9cdf9eb4f9282dd5114617f18b32bf0018e7d801
calvinkurniawann <calvinnkurniawan@gmail.com>
2023-03-29 13:17:15 +0700

    feat: detail resto

--- src/views/RestoView.vue
+++ src/views/RestoView.vue
@@ -1,6 +1,8 @@
 <script setup>
 import { useRestoRepository } from "@/composables";
 import { ref, onMounted } from "vue";
+import { RouterLink } from "vue-router";
+
 const repository = useRestoRepository();
 const isLoading = ref(true);
 const restos = ref([]);
@@ -25,16 +27,85 @@ const excerpt = (text, maxLenght = 10, indicator = "...") => {
 </script>
 
 <template>
+    <div class="">
+    <nav
+      class="
+        container
+        px-6
+        py-6
+        mx-auto
+        md:flex md:justify-between md:items-center
+      "
+    >
+      <div class="flex items-center justify-between">
+        <router-link
+          to="/"
+          class="
+            text-4xl
+            font-semibold
+            text-[#4FF8F4]
+            md:text-4xl "
+          >DishDash
+        </router-link>
+        <!-- Mobile menu button -->
+        <div @click="toggleNav" class="flex md:hidden">
+          <button
+            type="button"
+            class="
+              text-gray-100
+              hover:text-gray-400
+              focus:outline-none focus:text-gray-400
+            "
+          >
+            <svg viewBox="0 0 24 24" class="w-6 h-6 fill-current">
+              <path
+                fill-rule="evenodd"
+                d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
+              ></path>
+            </svg>
+          </button>
+        </div>
+      </div>
+
+      <!-- Mobile Menu open: "block", Menu closed: "hidden" -->
+      <ul
+        :class="showMenu ? 'flex' : 'hidden'"
+        class="
+          flex-col
+          mt-8
+          space-y-4
+          md:flex md:space-y-0 md:flex-row md:items-center md:space-x-10 md:mt-0
+        "
+      >
+        <router-link to="Home" ><li class="text-gray-100 hover:text-[#4FF8F4]">Home</li></router-link>
+        <router-link to="Home" ><li class="text-gray-100 hover:text-[#4FF8F4]">About</li></router-link>
+        <router-link to="Home" ><li class="text-gray-100 hover:text-[#4FF8F4]">Contact Us</li></router-link>
+        <router-link to="Home" ><li class="text-gray-100 hover:text-[#4FF8F4]">Create Resto</li></router-link>
+      </ul>
+    </nav>
+  </div>
   <div class="min-h-screen container mx-auto">
     <div class="grid grid-cols-12 gap-4 py-4">
       <div v-for="resto in restos" :key="resto.id" class="col-span-4">
         <!-- Card -->
-        <a href="#" class="block bg-white shadow p-4 rounded select-none">
+        <RouterLink
+          :to="{ name: 'restos-show', params: { id: resto.id } }"
+          class="block bg-white shadow p-4 rounded select-none"
+        >
           <h1 class="text-lg font-bold capitalize">{{ resto.name }}</h1>
           <p class="text-gray-500">{{ excerpt(resto.description, 40) }}</p>
-        </a>
+        </RouterLink>
       </div>
     </div>
   </div>
 </template>
-
+<script>
+import { ref } from 'vue';
+export default {
+  setup() {
+    let showMenu = ref(false);
+    const toggleNav = () => (showMenu.value = !showMenu.value);
+    return { showMenu, toggleNav };
+  },
+};
+</script>
